ae059a7db5f3cca421daa33925d0a13fbd0a2689
Nishtha Malhotra <nishthamalhotra@gmail.com>
2018-08-12 20:46:30 -0400

    React Setup

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
+const path = require('path');
 const express = require('express');
 const app = new express();
 
 module.exports = app;
 
+app.use(express.static(path.join(__dirname, "src")));
+app.use("/dist", express.static(path.join(__dirname, "dist")));
+
 app.use('/api/categories', require('./api/categories'));
 
 app.use((req, res, next) => {
-    next(new Error({message: "Could not find the resource", status: 404}));
+    let error = new Error("Could not find the resource");
+    error.status = 404;
+    next(error);
 });
 
 app.use((err, req, res, next) => {
-    console.log(err);
     res.status(err.status || 500).send(err.message || "Error occurred with the request");
 });
---
+++ src/Main.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Main extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Acme Products and Categories - React!</h1>
+            </div>
+        )
+    }
+}
+
+export default Main
